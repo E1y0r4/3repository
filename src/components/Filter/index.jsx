21bbac8305b8a414cf6625f2b0b836f6664ab13e
Zaynidin <zayniddinpolatov52@gmail.com>
2022-07-08 10:57:23 +0500

    popover

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import Input from '../Generic/Input'
-import { Container, Form } from './style'
+import { Container, Form, LogoInput,Icon,Odvanced } from './style'
 import Button from '../Generic/Button'
+import {  Popover } from 'antd';
+
 const Filter = () => {
+  const adsearch=<Odvanced>
+      <Odvanced.Title>address</Odvanced.Title>
+      <Odvanced.Title>apartment info</Odvanced.Title>
+      <Odvanced.Title>price</Odvanced.Title>
+  </Odvanced>
   return (
     <Container>
         <Form>
-            <Input placeholder={'Enter an address, neighborhood, city, or ZIP code'}></Input>
-            <Button ml={'10'} mr={'10'} width={'130px'} type={'secondary'}>Advanced</Button>
-            <Button width={'180px'} type={'primary'}>Search</Button>
+            <Input pl={'36px'} placeholder={'Enter an address, neighborhood, city, or ZIP code'}>
+              <LogoInput/>
+            </Input>
+            <Popover placement='bottomRight' content={adsearch} trigger={'click'} >
+            <Button ml={'20'} mr={'20'} width={'150px'} type={'secondary'}>
+              <Icon.AdvancedSetting/>Advanced
+              </Button>
+            </Popover>
+            <Button width={'180px'} type={'primary'}>
+              <Icon.Search/>Search
+              </Button>
         </Form>
     </Container>
   )
--- src/components/Filter/style.js
+++ src/components/Filter/style.js
@@ -1,18 +1,42 @@
 import styled from "styled-components";
 import { ReactComponent as homeInput } from "../../assets/icons/homeInput.svg";
+import { ReactComponent as  Advanced} from "../../assets/icons/advanced.svg";
+import { ReactComponent as Lupa } from "../../assets/icons/lupa.svg";
+
 const Container =styled.div`
     display: flex;
     justify-content: center;
 `
 const Icon =styled.div`
 `
+const LogoInput=styled(homeInput)`
+    margin-top:3px ;
+    margin-right:20px;
+`
+Icon.AdvancedSetting=styled(Advanced)`
+    margin-right: 5px;
+`
+Icon.Search=styled(Lupa)`
+    margin-right: 5px;
 
+`
 const Form =styled.div`
     display: flex;
     width: 100%;
     max-width: 1180px;
     padding: 10px 20px;
+    
+`
+const Odvanced=styled.div`
+    width: fit-content;
+    height:fit-content;
+`
+Odvanced.Title=styled.h2`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    text-transform: capitalize;
 `
 
-
-export {Container,Form}
+export {Container,Form ,LogoInput,Icon,Odvanced}
