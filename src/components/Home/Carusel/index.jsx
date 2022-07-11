66dfb067363f6b965326b69c41ba303f4adfd536
Aliakbar50 <akkigrovoy052@gmail.com>
2022-07-11 17:29:26 +0500

    swiper boldi

--- src/components/Home/Carusel/index.jsx
+++ src/components/Home/Carusel/index.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import {Container,Img,Icon}  from './style'
+import React, { useRef } from 'react'
+import {Container,Img,Icon, Wrapper}  from './style'
 import home1 from '../../../assets/imgs/home_1.png'
 import home2 from '../../../assets/imgs/home_2.png'
-import home4 from '../../../assets/imgs/home_4.png'
+
 
 
 
 const Carousel = () => {
+  const slider =useRef('click')
   return (
-    <>
-    <Icon.Left/>
-    <Icon.Right/>
+    <Wrapper>
+    <Icon.Left onClick={()=>slider.current.prev()}/>
+    <Icon.Right onClick={()=>slider.current.next()}/>
 
-    <Container autoplay>
+    <Container autoplay ref={slider}>
         <Img src={home1} alt="home" />
         <Img src={home2} alt="home" />
-        <Img src={home4} alt='home' />
+        <Img src={home1} alt="home" />
+        <Img src={home2} alt="home" />
+        
     </Container>
-    </>
+    </Wrapper>
     
   )
 }
--- src/components/Home/Carusel/style.js
+++ src/components/Home/Carusel/style.js
@@ -3,11 +3,13 @@ import { Carousel } from 'antd';
 import { ReactComponent as LeftArrow } from "../../../assets/icons/leftArrow.svg";
 import { ReactComponent as RightArrow } from "../../../assets/icons/rightArrow.svg";
 
+const Wrapper =styled.div`
+    position: relative;
+    height: 571px;
+` 
 
 const Container =styled(Carousel)`
     width: 100%;
-    height: 571px;
-    
 `
 const Img =styled.img`
     width: 100%;
@@ -20,16 +22,43 @@ Icon.Left =styled(LeftArrow)`
 display: flex;
 align-items: center;
 justify-content: center;
+position: absolute;
+background: black;
+border-radius: 50%;
+opacity: 0.5;
 top: 50%;
 left: 0px;
-transform: translate(100%,-50% );
+width: 45px;
+height: 45px;
+transform: translate(100%,-100% );
+z-index: 999;
+cursor: pointer;
+:hover{
+    opacity: 0.97;
+}
 `
 
 
 Icon.Right=styled(RightArrow)`
-
+display: flex;
+align-items: center;
+justify-content: center;
+position: absolute;
+background: black;
+opacity: 0.5;
+border-radius: 50%;
+z-index: 999;
+cursor: pointer;
+top: 50%;
+right: 0px;
+width: 45px;
+height: 45px;
+transform: translate(-100%,-100% );
+:hover{
+    opacity: 0.97;
+}
 `
 
 
 
-export {Container,Img,Icon} 
+export {Container,Img,Icon,Wrapper} 
